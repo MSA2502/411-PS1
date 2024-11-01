8e9703370a804746c0c04aee201ae69a7773dff9
Mohammed Sharjil Anwar <mohammedsharjilanwar@gmail.com>
2024-11-01 16:35:13 -0400

    updated kitchen.py file

--- docstrings_testing/meal_max/meal_max/models/kitchen_model.py
+++ docstrings_testing/meal_max/meal_max/models/kitchen_model.py
@@ -28,6 +28,16 @@ class Meal:
 
 
 def create_meal(meal: str, cuisine: str, price: float, difficulty: str) -> None:
+    """
+    creates a meal for the combatant
+    Args:
+        meal(str): the meal the combatant is having
+        cuisine(str): the type of cuisine the meal is(e.g Italian, Chinese, etc)
+        price(float): How much the meal costs
+        difficulty(str): how difficult the meal is to make
+    Returns:
+        returns nothing
+    """
     if not isinstance(price, (int, float)) or price <= 0:
         raise ValueError(f"Invalid price: {price}. Price must be a positive number.")
     if difficulty not in ['LOW', 'MED', 'HIGH']:
@@ -74,6 +84,13 @@ def clear_meals() -> None:
         raise e
 
 def delete_meal(meal_id: int) -> None:
+    """
+    deletes a meal
+    Args:
+        meal_id(int): the identifier of a meal
+    Returns:
+        returns nothing
+    """
     try:
         with get_db_connection() as conn:
             cursor = conn.cursor()
@@ -97,6 +114,13 @@ def delete_meal(meal_id: int) -> None:
         raise e
 
 def get_leaderboard(sort_by: str="wins") -> dict[str, Any]:
+    """
+    gets the leaderboard of combatants
+    Args:
+        sort_by(str): sorts combatants by number of wins
+    Returns:
+        returns a dictionary containing the combatant's meal_id, meal, cuisine, price, difficulty, battles, wins, and win_pct 
+    """
     query = """
         SELECT id, meal, cuisine, price, difficulty, battles, wins, (wins * 1.0 / battles) AS win_pct
         FROM meals WHERE deleted = false AND battles > 0
@@ -159,6 +183,13 @@ def get_meal_by_id(meal_id: int) -> Meal:
 
 
 def get_meal_by_name(meal_name: str) -> Meal:
+    """
+    gets a meal by its name
+    Args:
+        meal_name(str): the name of a meal
+    Returns:
+        returns a Meal object
+    """
     try:
         with get_db_connection() as conn:
             cursor = conn.cursor()
@@ -180,6 +211,14 @@ def get_meal_by_name(meal_name: str) -> Meal:
 
 
 def update_meal_stats(meal_id: int, result: str) -> None:
+    """
+    uptates the stats of a meal
+    Args:
+        meal_id: the id of a meal
+        result(str):
+    Returns:
+        returns nothing
+    """
     try:
         with get_db_connection() as conn:
             cursor = conn.cursor()
